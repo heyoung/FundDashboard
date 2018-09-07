583a5d0f8c2cc0ecf9af9a75687bcef9afb67ea3
Henry Young <henry.young155@gmail.com>
2018-09-07 22:47:10 +0100

    Refactor fund data manager logging

--- src/data/data-manager.ts
+++ src/data/data-manager.ts
@@ -1,10 +1,11 @@
 import { Db, MongoClient, UpdateWriteOpResult } from 'mongodb'
-import { Logger } from 'winston'
 import { buildLogger } from '../logging/logger-factory'
 import config from './db.config'
 import { FundData } from './fund-data'
 
 const MONGO_CONNECTION_URL = 'mongodb://localhost:27017/fund-dashboard'
+
+const logger = buildLogger('FundDataManager')
 
 class FundDataManager {
   public static async build(): Promise<FundDataManager> {
@@ -19,26 +20,24 @@ class FundDataManager {
 
   private db: Db
   private client: MongoClient
-  private logger: Logger
 
   constructor(client: MongoClient, db: string) {
     this.db = client.db(db)
     this.client = client
-    this.logger = buildLogger('FundDataManager')
   }
 
   public close(): Promise<void> {
-    this.logger.info('Closing mongo client connection')
+    logger.info('Closing mongo client connection')
     return this.client.close()
   }
 
   public getByIsin(isin: string): Promise<FundData | null> {
-    this.logger.info(`Getting fund data for isin: ${isin}`)
+    logger.info(`Getting fund data for isin: ${isin}`)
     return this.db.collection('funds').findOne({ isin })
   }
 
   public getAll(): Promise<FundData[]> {
-    this.logger.info('Getting all fund data')
+    logger.info('Getting all fund data')
     return this.db
       .collection('funds')
       .find()
@@ -46,7 +45,7 @@ class FundDataManager {
   }
 
   public save(data: FundData): Promise<UpdateWriteOpResult> {
-    this.logger.info(`Saving fund data: ${data.name}`)
+    logger.info(`Saving fund data: ${data.name}`)
     return this.db
       .collection('funds')
       .updateOne({ isin: data.isin }, { $set: data }, { upsert: true })
