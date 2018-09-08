0d26cbda097869a69f85f03d1fc7985dc0758dab
Henry Young <8026353+heyoung@users.noreply.github.com>
2018-09-09 00:24:24 +0100

    Remove unused code in scraper

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -1,17 +1,8 @@
 import { FundDataManager } from '../data/data-manager'
 import { Scraper } from './scraper'
 ; (async function main() {
-  // const SEC_IDS: string[] = ['F00000PI4A']
-  // const client: Client = new Client()
   const manager: FundDataManager = await FundDataManager.build()
-  // const pendingWritePromises: Promise<any>[] = []
-  // SEC_IDS.forEach(async id => {
-  //   pendingWritePromises.push(
-  //     client.getFundData(id).then(data => manager.save(data))
-  //   )
-  // })
-  // await Promise.all(pendingWritePromises)
-  // await manager.close()
+
   const scraper = new Scraper(manager)
 
   await scraper.scrape()
